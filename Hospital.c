3f34a9b907b64fba63c84bd2d41706f5781fcd82
Arthur Silva <93791425+AllbrightSl@users.noreply.github.com>
2025-02-17 11:03:51 -0300

    Melhorias

--- Hospital.c
+++ Hospital.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-<<<<<<< HEAD
 #include <ctype.h>
 
 #define MAX 100
@@ -9,7 +8,7 @@
 #define MAX_PACIENTE 50
 #define MAX_MEDICO 20
 typedef struct medico{
-    char registro_m[MAX];
+    char registro_m[MAX]; //Aqui estou deixando com 100 porque √© o tamanho m√°ximo que as strings ter√£o
     char nome_medico[MAX];
     char especialidade[MAX];
 }MEDICO;
@@ -23,39 +22,15 @@ typedef struct paciente{
     char sexo[MAX];
 }PACIENTE;
 typedef struct data{
-=======
-#include <locale.h>
-
-#define MAX_PACIENTE 50
-#define MAX_MEDICO 20
-#define MAX_CONSULTA 100 // Eu sÛ troquei os limites para ficar igual o do trabalho, mas deixar todos como 100 d· tambÈm
-
-typedef struct MEDICO{
-    int registro_m;  // Eu posso ter me enganado aqui, mas essa parte n„o È pra ser tipo o id? Pra mim o registro e o numero da consulta seriam o mesmo
-    char nome_medico[MAX_MEDICO];
-    char especialidade[MAX_MEDICO];
-}medico;
-
-typedef struct PACIENTE{
-    int registro_p;
-    char nome_paciente[MAX_PACIENTE];
-    char sexo[MAX_PACIENTE];
-    char endereco[MAX_PACIENTE];
-    int identidade;
-    int telefone;
-}paciente;
-typedef struct DATA{
->>>>>>> 018d3e6d775aab3b20bd3ce2d6f93adfec430f10
     int dia;
     int mes;
     int ano;
 }DATA;
 typedef struct consulta{
     int numero;
-<<<<<<< HEAD
-    MEDICO;
-    PACIENTE;
-    int horario;
+    MEDICO medico;
+    PACIENTE paciente;
+    char horario[6];
     int duracao;
     DATA;
 }CONSULTA;
@@ -64,30 +39,27 @@ typedef struct consulta{
     MEDICO M[MAX_MEDICO];
     CONSULTA C[MAX_CONSULTA];
 
-int buscar_consulta(char string[]){
-    for (int i = 0; i < 100; i++){
-        if(strcmp(string, C[i].) == 0){
-            return 1;
-        }
+    void consulta(){
+    /*Antes de j√° criar a consulta e colocar diretamente no nosso sistema, 
+    quero ver se o hor√°rio da consulta com o medico j√° est√° sendo usado
+    */    
+    printf("Escreva o numero de identificacao da consulta:\n");
+        scanf("");
+        printf("Escreva o nome do medico desejado:\n");
+        scanf("");
+        printf("Escreva o nome completo do paciente:\n");
+        scanf("");
+        printf("Escreva o horario desejado (no formato HH:MM):\n");
+        scanf("");
+        printf("Escreva a duracao necessaria para sua consulta:\n");
+        scanf("");
+        printf("Escreva a data: DD/MM/AA\n");
+        scanf(" %d/%d/%d", );
     }
-    return 0;
-}
-=======
-    medico m; //Aqui a gente precisa colocar o nome da variavel para poder acessar depois, tipo c1.m.nome_medico 
-    paciente p;
-    int horario;
-    int duracao;
-    data data;
-}consulta;
->>>>>>> 018d3e6d775aab3b20bd3ce2d6f93adfec430f10
 
 int main(){
-    setlocale(LC_ALL, "Portuguese"); // Para aceitar acentos em portuguÍs
-
     while(1){
-    int n, id=0, escolha;
-    consulta consultas[100];
-
+    int n;
     printf("----MENU----\n");
     printf("1 - Consulta\n");
     printf("2 - Paciente\n");
@@ -101,36 +73,7 @@ int main(){
 
     switch (n){
         case 1:
-<<<<<<< HEAD
-        char hora[6];
-        printf("Escreva o horario para sua consulta (formato HH:MM):\n");
-        scanf(" %6[^\n]", &hora);
-        if (buscar_consulta(hora)){
-            printf("Horario da consulta ja marcado!\n");
-            pritf("Favor marcar outro horario\n");
-        }
-        else{
-            
-        }
-
-=======
-        // Coloquei o primeiro switch case sendo a criaÁ„o geral da consulta, mas acho que a professora errou ao colocar aquele exemplo de menu. N„o tem o delete da consulta
-        consultas[id].numero = id;
-        printf("Escreva data para a sua consulta. Dia, MÍs e Ano: ");
-        scanf("%d %d %d", &consultas->data.dia, &consultas->data.mes, &consultas->data.ano );
-        printf("\nEscreva o horario para sua consulta. ‡s 3 horas, 6 horas ou 9 horas da manha?: ");
-        scanf("%d", &consultas->horario);
-        printf("\nEscreva a duraÁ„o para sua consulta. 30 minutos, 60 minutos ou 120 minutos?: ");
-        scanf("%d", &consultas->duracao);
->>>>>>> 018d3e6d775aab3b20bd3ce2d6f93adfec430f10
-
-        printf("\n\nConsulta feito para o dia %d do mÍs %d, ano %d, ‡s %d horas da manh„ com duraÁ„o de %d minutos\n\n",consultas[id].data.dia, consultas[id].data.mes, consultas[id].data.ano, consultas[id].horario, consultas[id].duracao);
-
-        printf("\nAgora para a informaÁ„o do paciente, forneÁa por favor o nome completo.");
-        fgets(consultas[id].p.nome_paciente, MAX_PACIENTE, stdin);
-        consultas->p.nome_paciente[strcspn(consultas->p.nome_paciente, "\n")] = 0;
-
-        id++;
+        consulta();
         break;
 
         case 2:
@@ -140,11 +83,10 @@ int main(){
         break;
         
         case 4:
-        // int escolha; Declarar essa variavel aqui tava bugando por algum motivo, coloquei ela l· pra cima.
-
+        int escolha;
         printf("Voce deseja:\n");
         printf("1 - Ver a lista de consultas agendadas para um paciente\n");
-        printf("2 - Ver a lista de consultas agendadas para um medico\n");
+        prinf("2 - Ver a lista de consultas agendadas para um medico\n");
         printf("3 - Ver a lista de pacientes para cada especialidade\n");
         scanf("%d", &escolha);
         
